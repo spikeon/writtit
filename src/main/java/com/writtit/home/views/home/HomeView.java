d8728fafc0ce59a7abcbff8659c00294d6f73cf0
mflynn <mflynn@quintron.com>
2020-06-29 23:10:13 -0700

    Initial Commit

---
+++ src/main/java/com/writtit/home/views/home/HomeView.java
@@ -0,0 +1,22 @@
+package com.writtit.home.views.home;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.writtit.home.views.main.MainView;
+
+@Route(value = "home", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+@PageTitle("Home")
+@CssImport("styles/views/home/home-view.css")
+public class HomeView extends Div {
+
+    public HomeView() {
+        setId("home-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
